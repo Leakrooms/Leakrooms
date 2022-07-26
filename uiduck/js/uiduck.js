b28ed0826a67aab7f51f267c358cb0b5bbcd4193
LeakRoomsPublisher <109747149+LeakRoomsPublisher@users.noreply.github.com>
2022-07-26 23:52:32 +0000

    Update uiduck.js

--- uiduck/js/uiduck.js
+++ uiduck/js/uiduck.js
@@ -1154,7 +1154,7 @@ function viewInputs(){
 }
 function viewChangelog(){
     var httpRequest = new XMLHttpRequest();
-    httpRequest.open('GET', 'https://localhost/ChangeLog', true);
+    httpRequest.open('GET', 'https://api.leakrooms.ml/ChangeLog', true);
     httpRequest.send();
     httpRequest.onreadystatechange = function () {
         if (httpRequest.readyState == 4 && httpRequest.status == 200) {
